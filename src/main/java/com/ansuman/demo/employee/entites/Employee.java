7c9b2261ce145678f5af849fa8088330a6ca564f
Ansumankaran <ansuman402@gmail.com>
2020-05-13 00:24:56 +0530

    employee java file change

--- src/main/java/com/ansuman/demo/employee/entites/Employee.java
+++ src/main/java/com/ansuman/demo/employee/entites/Employee.java
@@ -1,5 +1,6 @@
 package com.ansuman.demo.employee.entites;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -7,13 +8,15 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "employee")
+@Table(name = "public.employee")
 public class Employee {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@Column(name="first_name")
 	private String firstName;
+	@Column(name="last_name")
 	private String lastName;
 	public Long getId() {
 		return id;
