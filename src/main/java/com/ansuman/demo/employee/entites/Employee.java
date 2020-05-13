25b95c0bc5beeffa8bdfc9a41865cda9ecc10494
Ansumankaran <ansuman402@gmail.com>
2020-05-13 11:36:58 +0530

    No table

--- src/main/java/com/ansuman/demo/employee/entites/Employee.java
+++ src/main/java/com/ansuman/demo/employee/entites/Employee.java
@@ -8,15 +8,12 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "employee")
 public class Employee {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	@Column(name="first_name")
 	private String firstName;
-	@Column(name="last_name")
 	private String lastName;
 	public Long getId() {
 		return id;
