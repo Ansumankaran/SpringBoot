5cef6cd7e67c0264d0e86796727859bcabf2b928
Ansumankaran <ansuman402@gmail.com>
2020-05-15 21:01:31 +0530

    Cros error

--- src/main/java/com/ansuman/demo/employee/controllers/EmployeeController.java
+++ src/main/java/com/ansuman/demo/employee/controllers/EmployeeController.java
@@ -9,6 +9,7 @@ import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,6 +48,7 @@ public class EmployeeController {
 	
 	
 //	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@PostMapping(value="/add")
 	    public  Map<String, Object>  search(@RequestBody Employee request) {
 		    Employee emp =employeeRepository.findByFirstName(request.getFirstName());
